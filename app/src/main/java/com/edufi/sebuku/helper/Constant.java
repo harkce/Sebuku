c0c92e1536edffbfa19b6af985f6dd9d3e546a9c
harkce <habibgarut@gmail.com>
2016-03-05 07:29:14 +0700

    Tempat naro message-message & string-string constant

---
+++ app/src/main/java/com/edufi/sebuku/helper/Constant.java
@@ -0,0 +1,15 @@
+package com.edufi.sebuku.helper;
+
+/**
+ * Created by habib on 05/03/16.
+ * Semangat!!
+ */
+public class Constant {
+
+    public static final String HOST = "http://sebuku.vidukasi.com";
+    public static final String BLANKWARN = "Email/password tidak boleh kosong";
+    public static final String GREETINGS = "Selamat Datang!";
+    public static final String LOGINFAILED = "Login Gagal!";
+    public static final String ERRORMSG = "Terjadi Kesalahan";
+
+}
