52c98a61530c3c6c0fd89261cf9e5f10eddd5c90
harkce <habibgarut@gmail.com>
2016-03-05 07:28:12 +0700

    Fragment masih kosong

---
+++ app/src/main/java/com/edufi/sebuku/fragment/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.edufi.sebuku.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edufi.sebuku.R;
+
+/**
+ * Created by habib on 04/03/16.
+ * Semangat!!
+ */
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/edufi/sebuku/fragment/MessageFragment.java
@@ -0,0 +1,25 @@
+package com.edufi.sebuku.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edufi.sebuku.R;
+
+/**
+ * Created by habib on 04/03/16.
+ * Semangat!!
+ */
+public class MessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_message, container, false);
+        return rootView;
+    }
+
+}
